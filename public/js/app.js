dddc4b7c105ec841abc369715f4d5e958e49c8c3
utkarshg6 <utkarshabhinav.torres@gmail.com>
2020-04-27 16:22:27 +0100

    Basic Dropdown

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ const message = document.querySelector("#message");
 const displayImage = document.getElementById("display-image");
 const downloadSVG = document.getElementById("download-svg");
 const downloadPNG = document.getElementById("download-png");
+const iconNameDropdown = document.getElementById("icon-name-dropdown");
 const category = document.title.toLowerCase();
 
 const updateSVG = (name) => {
@@ -53,23 +54,37 @@ const fetchDetails = (category, iconName, colorCode, sizeInPixels) => {
 
 if (category == "regular") {
   fetchDetails("regular", "action_chains", "000000", "500");
+  iconNameDropdown.options.add(new Option(text = "Action Chains", value = "action_chains"));
+  iconNameDropdown.options.add(new Option(text = "Activate Subscriptions", value = "activate_subscriptions"));
+  iconNameDropdown.options.add(new Option(text = "Admin", value = "admin"));
+  iconNameDropdown.options.add(new Option(text = "Api", value = "api"));
+  iconNameDropdown.options.add(new Option(text = "Application Incomplete", value = "application_incomplete"));
+  iconNameDropdown.options.add(new Option(text = "Application Instance", value = "application_instance"));
 }
 
 if (category == "material") {
   fetchDetails("material", "wifi", "000000", "500");
+  iconNameDropdown.options.add(new Option(text = "3d Rotation", value = "3d_rotation"));
+  iconNameDropdown.options.add(new Option(text = "Ac Unit", value = "ac_unit"));
+  iconNameDropdown.options.add(new Option(text = "Access Alarm", value = "access_alarm"));
+  iconNameDropdown.options.add(new Option(text = "Access Alarms", value = "access_alarms"));
+  iconNameDropdown.options.add(new Option(text = "Access Time", value = "access_time"));
 }
 
 if (category == "animated") {
   fetchDetails("animated", "installing", "000000", "500");
   downloadPNG.hidden = true;
+  iconNameDropdown.options.add(new Option(text = "Installing", value = "installing"));
+  iconNameDropdown.options.add(new Option(text = "Loading", value = "loading"));
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const iconName = document.querySelector("#iconName").value;
-  const colorCode = document.querySelector("#colorCode").value.slice(1, 7);
-  const sizeInPixels = document.querySelector("#sizeInPixels").value;
+  const iconName = document.querySelector("#icon-name-dropdown").value;
+  const colorCode = document.querySelector("#color-code").value.slice(1, 7);
+  const sizeInPixels = document.querySelector("#size-in-pixels").value;
   console.log(category);
+  console.log(iconName);
   // message.textContent = "Loading...";
 
   fetchDetails(category, iconName, colorCode, sizeInPixels);
