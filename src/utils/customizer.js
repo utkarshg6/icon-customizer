3b0d051082d93ed890d49c1795c58af5cfc90ab7
utkarshg6 <utkarshabhinav.torres@gmail.com>
2020-04-30 18:04:32 +0100

    Rmove redundant code

--- src/utils/customizer.js
+++ src/utils/customizer.js
@@ -1,31 +1,24 @@
 const fs = require("fs");
 
 
-modifySVG = (svgCode, colorCode, sizeInPixels) => {
+customizeSVG = (svgCode, colorCode, sizeInPixels) => {
 
   if (svgCode.search('width="24" height="24"')) {
     svgCode = svgCode.replace('width="24" height="24"', '');
   }
 
-  const index1 = [...svgCode.matchAll(/>/g)][0]["index"] + 1;
-  const arr = [...svgCode.matchAll(/</g)];
-  const index2 = arr[arr.length - 1]["index"];
+  // Index of Closing Bracket
+  const index = [...svgCode.matchAll(/>/)][0]["index"] + 1;
 
-  const finalSVG =
-    svgCode.slice(0, index1 - 1) +
-    ` width="${sizeInPixels}px" height="${sizeInPixels}px">` +
-    `<g fill="#${colorCode}">` +
-    svgCode.slice(index1, index2) +
-    "</g>" +
-    svgCode.slice(index2, svgCode.length);
-
-  return finalSVG;
+  return svgCode.slice(0, index - 1) +
+    ` fill="#${colorCode}" width="${sizeInPixels}px" height="${sizeInPixels}px">` +
+    svgCode.slice(index, svgCode.length);
 };
 
 generateJSON = (category, iconName, colorCode, sizeInPixels) => {
   const svgFile = "./public/img/svg/" + category + "/" + iconName + ".svg";
   const svgCode = fs.readFileSync(svgFile, "utf8");
-  const modifiedSvgCode = modifySVG(svgCode, colorCode, sizeInPixels);
+  const modifiedSvgCode = customizeSVG(svgCode, colorCode, sizeInPixels);
 
   finalJSON = {};
 
